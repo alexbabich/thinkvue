02274ffa66145e9ce2d89de74994590d3699238f
alexbabich <ababych.m@gmail.com>
2017-12-14 11:21:42 +0200

    little hotfix from feedback and add dist archive

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -2,7 +2,7 @@
     <b-container class="text-center pt-5 pb-5">
         <p class="th-block-title">Отзывы</p>
         <ul class="list-unstyled row th-testimonial-list">
-            <li v-for="testimonial in testimonialsList" :key="testimonial.id" class="col-8 col-md-6 m-auto th-testimonial-item">
+            <li v-for="testimonial in testimonialsList" :key="testimonial.id" class="col-md-6 col-xs-10 m-auto th-testimonial-item">
                 <img v-if="testimonial.img" class="rounded-circle mb-4 th-user-photo" :src="testimonial.img" :alt="testimonial.user"/>
                 <span v-else class="rounded-circle mb-4 th-user-photo th-default-photo"></span>
                 <p class="th-testimonial-title">{{testimonial.user}}</p>
@@ -20,7 +20,7 @@
         testimonialsList: [
           {
             img: './static/img/user.jpg',
-            user: 'Александр Николаенко, со-основатель суши бара Shifu',
+            user: 'Алексей Чечель, со-основатель суши бара Shifu',
             text: 'Используя приложение Shifu удалось увеличить прибыль на 25% в течении первого месяца. Ежедневно приложение используют более 100 клиентов. Общий обьем заказов через приложение составляет около 30%.'
           }
         ]
